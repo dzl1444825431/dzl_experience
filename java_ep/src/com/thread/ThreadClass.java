bc35b9e865ad3e737003f1fd8360276ba16de554
dzl <1444825431@qq.com>
2015-10-21 16:32:14 +0800

    1.java thread线程深入研究第二天：了解了线程的特征内容

--- java_ep/src/com/thread/ThreadClass.java
+++ java_ep/src/com/thread/ThreadClass.java
@@ -1,10 +1,15 @@
 package com.thread;
 
+import java.util.Collection;
+import java.util.TimerTask;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Exchanger;
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -13,11 +18,17 @@ import java.util.concurrent.locks.ReentrantLock;
 
 import javax.management.monitor.Monitor;
 
+import org.apache.http.annotation.NotThreadSafe;
+import org.apache.http.annotation.ThreadSafe;
+//@ThreadSafe
+//@NotThreadSafe
 public class ThreadClass {
-	
+
 	public static void main(String[] args) {
 		
 		Executors e;//线程池执行者
+		Executor executor;
+		
 		CyclicBarrier c;//屏障
 		Thread thread;
 		Lock lock;//锁
@@ -38,6 +49,18 @@ public class ThreadClass {
 		Exchanger<?> exchanger;
 		
 		ReadWriteLock readWriteLock;//读写锁
+		
+		Collection<?> collection;
+		
+		ThreadPoolExecutor threadPoolExecutor;//线程池
+		
+		TimerTask task;
+		
+		ScheduledThreadPoolExecutor scheduledThreadPoolExecutor;
+		
+		ThreadDeath threadDeath;
+		
+		
 		
 	}
 
