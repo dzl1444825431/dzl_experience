80d8f01b3ecc451b0ba5bee91b20fea9f75c0f17
dzl <1444825431@qq.com>
2016-03-02 17:59:39 +0800

    1.

--- groovy_ep/src/com/dzl/groovy/生成xml转Android.groovy
+++ groovy_ep/src/com/dzl/groovy/生成xml转Android.groovy
@@ -1,7 +1,7 @@
 package com.dzl.groovy
 
 
-def xmlFilePath = "D:\\baiduYun\\dzl_github\\app1\\yiqiaoquancheng\\res\\layout\\item_footer.xml"
+def xmlFilePath = "D:\\baiduYun\\dzl_github\\dzl_experience\\groovy_ep\\src\\com\\dzl\\groovy\\item_footer.xml"
 def xml = new XmlParser().parse(xmlFilePath)
 
 //println xml
@@ -96,12 +96,40 @@ int i = 0
 //	println it instanceof Node
 //}
 
-int j = 1
+int index = 1
 
 //1.读取和设置属性  2.设置布局参数  3.添加到父view中
 
+//注意引用, 背景 padding
+
+def viewMethod = [
+	'id':'setId',//(int)
+	'tag':'setTag',//(Object)
+//	'background':'setBackground',//(Drawable)
+//	'background':'setBackgroundColor',//(int)
+//	'background':'setBackgroundDrawable',//(Drawable)
+//	'background':'setBackgroundResource',//(int)
+	
+//	'padding':'setPadding',//(int, int, int, int)
+//	'paddingLeft':'setPadding',//(int, int, int, int)
+//	'paddingTop':'setPadding',//(int, int, int, int)
+//	'paddingRight':'setPadding',//(int, int, int, int)
+//	'paddingBottom':'setPadding',//(int, int, int, int)
+	
+	'focusable':'setFocusable',//(boolean)
+	'visibility':'setVisibility',//(int)
+	'clickable':'setClickable',//(boolean)
+	'alpha':'setAlpha',//(float)
+	'minHeight':'setMinimumHeight',//(int)
+	'minWidth':'setMinimumWidth',//(int)
+	'enabled':'setEnabled',//(boolean)
+	'selected':'setSelected',//(boolean)
+	
+	]
+
+
 def each3
-each3 = {node, level, parentname ->
+each3 = {node, level, parentNodeName, parentVariableName ->
 
 	if (!node) {
 		return
@@ -110,36 +138,151 @@ each3 = {node, level, parentname ->
 	println ""
 	
 	level++
+	index++
+	
 	def nodeName = node.name()
-	def nodeVariableName = nodeName.toLowerCase() + "_" + level + "_" + j++
+	def nodeVariableName = nodeName.toLowerCase() + "_${level}_${index}"
 	//	println level + " name = " + nodeName
 
-	println nodeName + " " + nodeVariableName + " = new " + nodeName + "(context);"
+	println "${nodeName} ${nodeVariableName} = new ${nodeName}(context);"
+	
+	def attrs_layout = [:];
+	def attrs_self = [:];
+	
 	node.attributes().each {
-		def attr = it.key.toString().replace("{http://schemas.android.com/apk/res/android}","")
+		def attr = it.key.toString().substring(it.key.toString().indexOf("}") + 1)
+		//.replace("{http://schemas.android.com/apk/res/android}","")
 		
-		if (attr == "layout_width") {
-			attr = attr[0].toUpperCase() + attr.substring(1)
-			println "${parentname}.LayoutParams params = new ${parentname}.LayoutParams(, heigth);"
+		def val = it.value.toString()
+		if(val.startsWith("@")){
+			//处理引用value
+			def ind = val.indexOf("/") + 1
+			if (val.startsWith("@anim")) {
+				val = "res.getAnimation(R.anim." + val.substring(ind) + ")"
+			}else if (val.startsWith("@color")) {
+				val = "res.getColor(R.color." + val.substring(ind) + ")"
+			}else if (val.startsWith("@dimen")) {
+				val = "res.getDimension(R.dimen." + val.substring(ind) + ")"
+			}else if (val.startsWith("@drawable")) {
+				val = "res.getDrawable(R.drawable." + val.substring(ind) + ")"
+			}else if (val.startsWith("@layout")) {
+				val = "res.getLayout(R.layout." + val.substring(ind) + ")"
+			}else if (val.startsWith("@string")) {
+				val = "res.getString(R.string." + val.substring(ind) + ")"
+			}else{
+				val = "R.id." + val.substring(ind)
+			}
+		}else if(val.startsWith("#")){
+			//处理颜色
+			if(val.size() == 9){
+				val = val.replace("#","0x")
+			}else if(val.size() == 7){
+				val = val.replace("#","0xff")
+			}else if(val.size() == 4){
+				val = "0xff" + val[1] + val[1] + val[2] + val[2] + val[3] + val[3]
+			}
+		}else if (val == "visible" || val == "gone") {
+			val = val[0].toUpperCase()
+		}
+		
+		//处理布局属性
+		if (attr.startsWith("layout_")) {
+			attrs_layout.put(attr, val)
 		} else {
-		
-			attr = attr[0].toUpperCase() + attr.substring(1)
-					println nodeVariableName + ".set" + attr + "(" + it.value + ");"
+			attrs_self.put(attr, val)
 		}
 		
 	}
+	def paramName = "params_${level}_${index}"
+	println "${parentNodeName}.LayoutParams ${paramName} = new ${parentNodeName}.generateDefaultLayoutParams();"
+	
+	//解析布局属性
+	attrs_layout.each {key, value->
+		if (value == "match_parent" || value == "wrap_content") {
+			value = value.toUpperCase()
+		}
+		println "${paramName}.${key} = ${value};"
+	}
+	println "${nodeVariableName}.setLayoutParams(${paramName});"
+	println ""
+	
+	def padding = [:]
+	
+	//开始解析self自身属性
+	def iterator = attrs_self.entrySet().iterator()
+	while (iterator.hasNext()) {
+		def entry = iterator.next()
+		
+		if (entry.key == 'background'){
+			if (entry.value.contains('R.drawable')) {
+				println "${nodeVariableName}.setBackground(${entry.value});"
+			}else{
+				println "${nodeVariableName}.setBackgroundColor(${entry.value});"
+			}
+		}else if(entry.key.startsWith('padding')){
+			//解析padding
+			padding.put(entry.key, entry.value)
+		}else {
+		
+			viewMethod.each {
+				
+				if (it.key == entry.key) {
+					
+					iterator.remove()
+					println "${nodeVariableName}.${it.value}(${entry.value});"
+					
+					return;
+				}
+			}
+		}
+		
+	}
+	
+	def paddingLeft = 0
+	def paddingTop = 0
+	def paddingRight = 0
+	def paddingBottom = 0
+	//解析生成padding参数
+	padding.each {
+		if (it.key == 'padding') {
+			paddingLeft = paddingTop = paddingRight = paddingBottom = it.value
+			return
+		}else if (it.key == 'paddingLeft') {
+			paddingLeft = it.value
+		}else if (it.key == 'paddingTop') {
+			paddingTop = it.value
+		}else if (it.key == 'paddingRight') {
+			paddingRight = it.value
+		}else if (it.key == 'paddingBottom') {
+			paddingBottom = it.value
+		}
+	}
+	
+	if (padding) {
+		println "${nodeVariableName}.setPadding(${paddingLeft},${paddingTop},${paddingRight},${paddingBottom});"
+	}
+	
+//	//解析self属性
+//	attrs_self.each {
+//		
+//		println attrs_self.size()
+//		println "11111111${nodeVariableName}.setAttr(${it.key},${it.value});"
+//		it.remove()
+//	}
+	
+	println ((parentVariableName ? "${parentVariableName}." : "") + "addView(${nodeVariableName});")
 	
 //	RecyclerView.LayoutParams params = new RecyclerView.LayoutParams(width, heigth);
 //	footer_status_view.setLayoutParams(params);
 //	rcl_product.addFooterView(footer_status_view);
 
 	node.children().each { child ->
-		each3(child, level, nodeName)
+		each3(child, level, nodeName, nodeVariableName)
 	}
 
 }
 
-each3(xml, 0, "")
+each3(xml, 0, "ViewGroup", "")
 
 
 
