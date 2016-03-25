35e2df7b9f15e2cfea80addabf8083c3bc24bb40
dzl <1444825431@qq.com>
2016-03-25 18:02:09 +0800

    1

--- groovy_ep/src/com/dzl/groovy/apk/读取APK信息groovy.groovy
+++ groovy_ep/src/com/dzl/groovy/apk/读取APK信息groovy.groovy
@@ -1,41 +1,51 @@
 package com.dzl.groovy.apk
 
 
-def file_path = 'D:\\D\\16_03_23'
-def file_path_log = 'D:\\aaa2.txt'
+def file_path = 'D:\\D\\16_03_22'
+def file_path_log = 'D:\\aaa4.txt'
 
 def f = new File(file_path)
 File f_log = new File(file_path_log)
 
-def file_filter = new FileFilter(){
-	boolean accept(File arg0) {
-		return arg0.getName().endsWith('.apk')
+readApkAndWrite(f, f_log)
+
+//println f_log.getText
+
+private readApkAndWrite(File f, File f_log) {
+	def file_filter = new FileFilter(){
+				boolean accept(File arg0) {
+					return arg0.getName().endsWith('.apk')
+				}
+			}
+
+	def list = f.listFiles(file_filter)
+	def file_lists = []
+	list.each {
+		file_lists.add(it.getAbsolutePath())
+	}
+
+	println file_lists
+	String aapt = 'aapt d badging '
+	if (!f_log.exists()) {
+		f_log.createNewFile()
+	}
+
+	Runtime runtime = Runtime.getRuntime()
+	file_lists.each {
+		try {
+			String cmd = aapt + it
+			def progress = runtime.exec(cmd)
+			f_log.append('\r\n')
+			f_log.append(progress.getInputStream())
+
+		} catch (Exception e) {
+			e.printStackTrace()
+		}
 	}
 }
 
-def list = f.listFiles(file_filter)
-def file_lists = []
-list.each {
-	file_lists.add(it.getAbsolutePath())
-}
-
-println file_lists
-String aapt = 'aapt d badging '
-if (!f_log.exists()) {
-	f_log.createNewFile()
-}
-
-Runtime runtime = Runtime.getRuntime()
-file_lists.each {
-	try {
-		String cmd = aapt + it
-		def progress = runtime.exec(cmd)
-		f_log.append('\r\n')
-		f_log.append(progress.getInputStream())
-		
-	} catch (Exception e) {
-		e.printStackTrace()
+f_log.eachLine {
+	if (it.contains('application-label:')) {
+		println it
 	}
 }
-
-println f_log.getText()
--- groovy_ep/src/com/dzl/groovy/net/递归文件.groovy
+++ groovy_ep/src/com/dzl/groovy/net/递归文件.groovy
@@ -1,10 +1,11 @@
 package com.dzl.groovy.net
 
-def path = 'D:\\D\\16_03_22'
+def path = 'D:\\D\\16_03_23'
 def file = new File(path)
 def list = file.list()
+println 'adb shell pm set-install-location 2 '
 list.each {
 	if (it.endsWith('.apk')) {
-		println "adb install ${path}\\${it}"
+		println "adb install -s ${path}\\${it}"
 	}
 }
